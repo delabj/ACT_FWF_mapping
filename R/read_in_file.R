bb3a41d85b3f7b3f26bfcb5fcd421872c7884b9d
Joshua de la Bruere <j.l.delabruere@gmail.com>
2020-02-28 08:06:42 -0600

    Initial commit
    Reworking my ACT file reading package to follow better naming conventions and package conventions.

---
+++ R/read_in_file.R
@@ -0,0 +1,57 @@
+#' Read in the ACT fixed width file
+#'
+#' @param file The file path to the file
+#' @param year The school year for the mapping
+#' @param blanks Boolean option to include the blank columns in the resulting data frame
+#' @param scores_only Boolean option to only select scores.
+#' @return A data frame containing the data from the fixed width file provided by the ACT
+#' @examples
+#'
+#' read_ACT(filepath, mapping="19-20")
+
+
+read_ACT<- function(file = "fileName", year="19-20", blanks=F, scores_only=T){
+  df <- GetMapping(year)
+  if(!is.data.frame(df)){
+    stop("Given Mapping isn't a dataframe. Have you entered the year correctly?")
+  }
+
+  #reads in the file
+  temp <- read.fwf(file=file, widths = df$widthsACT, col.names = df$namesACT)
+  # checks to see if blank columns are needed
+  if(blanks){return(temp)}
+  #if not  removes the columns named blank
+  temp %>%
+    select(-(starts_with("blank")))->temp
+
+  #only grab student names and scores
+  if(scores_only){
+    temp %>%
+      select(lastName, firstName, dateOfBirth, stateID, testDate,
+             "composite"= scaleComposite,
+             "english" = scaleEnglish,
+             "mathmatics"=scaleMath,
+             "reading"= scaleReading,
+             "science"= scaleScience,
+             "writing subscore"= scoreWR
+      )->temp
+  }
+
+  return(temp)
+}
+
+
+#' Select the mapping to use when reading the ACT fixed width file
+#'
+#' @param year The school year for the mapping
+#' @return A data frame containing column names and widths
+
+
+GetMapping <- function(year="19-20"){
+  switch(year,
+         "18-19"= return(actMap18_19),  #sep2018-aug2019
+         "19-20"= return(actMap19_20),  #sep2019-aug2020
+         stop('No Mapping found, have you entered the year correctly?') #error catching
+  )
+  return(NULL)
+}
