bb5df10079469a0bb63784587c40796023f2a434
Joshua de la Bruere <j.l.delabruere@gmail.com>
2020-02-28 12:07:48 -0600

    Styled read_in_file

--- R/read_in_file.R
+++ R/read_in_file.R
@@ -7,32 +7,34 @@
 #' @return A data frame containing the data from the fixed width file provided by the ACT
 #' @examples
 #'
-#' read_ACT(filepath, mapping="19-20")
-read_ACT<- function(file = "fileName", year="19-20", blanks=F, scores_only=T){
+#' #read_ACT(filepath, mapping = "19-20")
+read_ACT <- function(file = "fileName", year = "19-20", blanks = F, scores_only = T) {
   df <- get_mapping(year)
-  if(!is.data.frame(df)){
+  if (!is.data.frame(df)) {
     stop("Given Mapping isn't a dataframe. Have you entered the year correctly?")
   }
 
-  #reads in the file
-  temp <- read.fwf(file=file, widths = df$widthsACT, col.names = df$namesACT, comment.char="")
+  # reads in the file
+  temp <- read.fwf(file = file, widths = df$widthsACT, col.names = df$namesACT, comment.char = "")
   # checks to see if blank columns are needed
-  if(blanks){return(temp)}
-  #if not  removes the columns named blank
+  if (blanks) {
+    return(temp)
+  }
+  # if not  removes the columns named blank
   temp %>%
-    dplyr::select(-(dplyr::starts_with("blank")))->temp
+    dplyr::select(-(dplyr::starts_with("blank"))) -> temp
 
-  #only grab student names and scores
-  if(scores_only){
+  # only grab student names and scores
+  if (scores_only) {
     temp %>%
       dplyr::select(lastName, firstName, dateOfBirth, stateID, testDate,
-             "composite"= scaleComposite,
-             "english" = scaleEnglish,
-             "mathmatics"=scaleMath,
-             "reading"= scaleReading,
-             "science"= scaleScience,
-             "writing subscore"= scoreWR
-      )->temp
+        "composite" = scaleComposite,
+        "english" = scaleEnglish,
+        "mathmatics" = scaleMath,
+        "reading" = scaleReading,
+        "science" = scaleScience,
+        "writing subscore" = scoreWR
+      ) -> temp
   }
 
   return(temp)
@@ -43,11 +45,11 @@ read_ACT<- function(file = "fileName", year="19-20", blanks=F, scores_only=T){
 #'
 #' @param year The school year for the mapping
 #' @return A data frame containing column names and widths
-get_mapping <- function(year="19-20"){
+get_mapping <- function(year = "19-20") {
   switch(year,
-         "18-19"= return(actMap18_19),  #sep2018-aug2019
-         "19-20"= return(actMap19_20),  #sep2019-aug2020
-         stop('No Mapping found, have you entered the year correctly?') #error catching
+    "18-19" = return(actMap18_19), # sep2018-aug2019
+    "19-20" = return(actMap19_20), # sep2019-aug2020
+    stop("No Mapping found, have you entered the year correctly?") # error catching
   )
   return(NULL)
 }
